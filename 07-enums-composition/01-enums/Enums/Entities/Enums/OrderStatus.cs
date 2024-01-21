1e4618927b071371ac99cf56272853d40b5a1997
sandersonrafael <sandersonrafael-35@hotmail.com>
2024-01-21 15:34:27 -0300

    Enums

---
+++ 07-enums-composition/01-enums/Enums/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+namespace Enums.Entities.Enums
+{
+    public enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ 07-enums-composition/01-enums/Enums/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Enums.Entities;
+using Enums.Entities.Enums;
+
+Order order = new Order { Id = 1080, Moment = DateTime.Now, Status = OrderStatus.PendingPayment };
+
+Console.WriteLine(order);
+
+/* Conversion */
+
+// Enum to String
+string txt = OrderStatus.PendingPayment.ToString();
+Console.WriteLine(txt);
+
+// String to Enum
+OrderStatus orderStatus = Enum.Parse<OrderStatus>("Shipped");
+Console.WriteLine(orderStatus);
