65104dd21ec49a67d91653cb8c12d3338191036e
sandersonrafael <sandersonrafael-35@hotmail.com>
2024-01-24 13:20:10 -0300

    HashSet & SortedSet | Dictionary & SortedDictionary

---
+++ 12-generic-set-dictionary/03-hashset-sortedset/HashSetSortedSet/Program.cs
@@ -0,0 +1,86 @@
+﻿/* HashSet && SortedSet */
+
+// Represents a set of elements (similar to algebra)
+// Don't alow repetitions
+// No positions defined
+// Fast access, insertion and remotion of elements
+// Offer efficient set operations: intersection, union and difference
+
+/* HashSet */
+
+// Hash table storage
+// Extremely fast: O(1) insertion, removal and search
+// The order of elements is not guaranteed
+
+/* SortedSet */
+
+// Tree storage
+// Fast: insertion, removal and search O(log(n))
+// Elements are stored in order according to the IComparer<T> implementation
+
+/* Methods */
+
+// Add
+// Clear
+// Contains
+// UnionWith(other)
+// IntersectWith(other)
+// ExceptWith(other)
+// Remove(T)
+// RemoveWHere(predicate)
+
+
+/* HashSet Example */
+
+HashSet<string> stringSet = new HashSet<string>();
+
+stringSet.Add("TV");
+stringSet.Add("Notebook");
+stringSet.Add("Tablet");
+
+Console.WriteLine("stringSet: ");
+PrintCollection(stringSet);
+
+Console.WriteLine($"stringSet.Contains(\"Notebook\"): {stringSet.Contains("Notebook")}");
+Console.WriteLine($"stringSet.Contains(\"Computer\"): {stringSet.Contains("Computer")}");
+
+
+/* SortedSet Example */
+
+SortedSet<int> intSetA = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
+SortedSet<int> intSetB = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
+
+Console.WriteLine("\nintSetA: ");
+PrintCollection(intSetA);
+
+Console.WriteLine("intSetB: ");
+PrintCollection(intSetB);
+
+// Union
+SortedSet<int> intSetC = new SortedSet<int>(intSetA);
+Console.WriteLine("intSetC: // Union ");
+intSetC.UnionWith(intSetB);
+PrintCollection(intSetC);
+
+// Intersection
+SortedSet<int> intSetD = new SortedSet<int>(intSetA);
+Console.WriteLine("intSetD: // Intersection ");
+intSetD.IntersectWith(intSetB);
+PrintCollection(intSetD);
+
+// Difference
+SortedSet<int> intSetE = new SortedSet<int>(intSetA);
+Console.WriteLine("intSetE: // Difference ");
+intSetE.ExceptWith(intSetB);
+PrintCollection(intSetE);
+
+// IEnumerable is an Interface implemented by all collections from System.Collections.Generic
+static void PrintCollection<T>(IEnumerable<T> collection)
+{
+    Console.Write('[');
+    foreach (T item in collection)
+    {
+        Console.Write($" {item} ");
+    }
+    Console.Write("]\n\n");
+}
---
+++ 12-generic-set-dictionary/04-dictionary-sorteddictionary/DictionarySortedDictionary/Program.cs
@@ -0,0 +1,56 @@
+﻿/* Dictionary && SortedDictionary */
+
+// In another languages, is commonly called by Map
+// Is a generic collection that receives a pair of key and value, by different (optional) types both
+// The key can't be repeated
+// Elements indexed by they key (they don't have specified position)
+// Fast access, insertion and remotion of elements
+
+/* Dictionary */
+
+// Stores in a hash table
+// Extremely fast / Insertion, remotion and search in O(1)
+// Element order don't be granted
+
+
+/* SortedDictionary */
+
+// Stores in a tree
+// Fast / Insertion, remotion and search in O(log(n))
+// The elements are stored in order according to the IComparer<T> implementation
+
+/* Methods */
+
+// Add(key, value)
+// Clear()
+// Count
+// ContainsKey(key)
+// ContainsValue(value)
+// Remove(key)
+
+Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+// It's possible to add and access values with brackets notation too
+
+cookies["user"] = "Maria";
+cookies["email"] = "maria@gmail.com";
+cookies["phone"] = "12345678910";
+cookies["phone"] = "98765432101"; // override
+
+Console.WriteLine(cookies["email"]);
+Console.WriteLine(cookies["phone"]);
+
+cookies.Remove("user");
+
+if (cookies.ContainsKey("user")) Console.WriteLine(cookies["user"]);
+else Console.WriteLine("Key \"user\" don't exists in Dictionary");
+
+Console.WriteLine($"cookies.Count: {cookies.Count}");
+
+Console.WriteLine("Dictionary pairs: ");
+Console.Write('[');
+foreach (KeyValuePair<string, string> item in cookies)
+{
+    Console.Write($" \"{item.Key}\": \"{item.Value}\" ");
+}
+Console.WriteLine(']');
